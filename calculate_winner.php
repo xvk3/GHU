008f55e994d2717f56d4598b7b0298a9abc0ea5d
W <w@xvk3.net>
2020-05-01 10:09:35 +0100

    calculates highest unique guess, retrieves the winning participants name, email and token

--- calculate_winner.php
+++ calculate_winner.php
@@ -41,6 +41,101 @@
     // Update META
     $sql = "UPDATE META SET STATE=3, FP=$fp WHERE TRUE";
     mysqli_query($conn, $sql);
+    // TODO change the above to use prepared statements
+    
+    $sql = "SELECT ID,GUESS FROM GHU WHERE TRUE";
+    $pql = mysqli_prepare($conn, $sql);
+    if(!mysqli_stmt_execute($pql)) {
+      echo "calculate_winner.php:mysqli_stmt_execute failed\r\n";
+      //die();
+    } else {
+      //echo "calculate_winner.php:mysqli_stmt_execute success\r\n";
+    }
+
+    $res = mysqli_stmt_get_result($pql);
+    if(!$res) {
+      echo "calculate_winner.php:mysqli_stmt_get_result failed\r\n";
+      //die();
+    } else {
+      //echo "calculate_winner.php:mysqli_stmt_get_result success\r\n";
+    }
+    $nr = $res->num_rows;
+
+    $arr = array();
+    if($nr > 0) {
+      while($row = mysqli_fetch_assoc($res)) {
+        $arr[] = $row['GUESS'];
+      }
+    } else {
+      echo "calculate_winner.php:num_rows <= 0\r\n";
+    }
+
+    //var_dump($arr);
+    rsort($arr);
+    //var_dump($arr);
+
+    $highestUnique = 0;
+    $skipGuess = 0;
+    // Loop over the sorted array (highest guesses first)
+    for($i = 0; $i < count($arr); $i++) {
+      // Make sure we're not at the end of the array, also as soon as highestUnique is set we can exit the loop
+      if($i != (count($arr)-1) && $highestUnique == 0) {
+        //echo "i = " . $i . ", arr[i] = " . $arr[$i] . ", arr[i+1] = " . $arr[$i+1] . "\r\n";
+        // Skip duplicate guesses
+        if($arr[$i] == $arr[$i+1]) $skipGuess = $arr[$i];
+        if($skipGuess != $arr[$i]) {
+          if($arr[$i] > $highestUnique && $arr[$i] != $arr[$i+1]) {
+            //echo "setting highestUnique = " . $highestUnique . ", to = " . $arr[$i] . "\r\n";
+	    $highestUnique = $arr[$i];
+          }
+        }
+      }
+    }
+
+    echo $highestUnique . "\r\n";
+
+    $sql = "SELECT NAME,EMAIL,TOKEN FROM GHU WHERE GUESS=?";
+    $pql = mysqli_prepare($conn, $sql);
+    if(!mysqli_stmt_bind_param($pql, 's', $highestUnique)) {
+      echo "calculate_winner.php:mysqli_stmt_bind_param failed\r\n";
+      //die();
+    } else {
+      //echo "calculate_winner.php:mysqli_stmt_bind_param success\r\n";
+    }
+
+    if(!mysqli_stmt_execute($pql)) {
+      echo "calculate_winner.php:mysqli_stmt_execute failed\r\n";
+      //die();
+    } else {
+      //echo "calculate_winner.php:mysqli_stmt_execute success\r\n";
+    }
+
+    $res = mysqli_stmt_get_result($pql);
+    if(!$res) {
+      echo "calculate_winner.php:mysqli_stmt_get_result failed\r\n";
+      //die();
+    } else {
+      //echo "calculate_winner.php:mysqli_stmt_get_result success\r\n";
+    }
+
+    if($res->num_rows == 0) {
+      echo "calculate_winner.php:num_rows == 0\r\n";
+      //die();
+    } else {
+      //echo "calculate_winner.php:num_rows > 0\r\n";
+    }
+
+    $row = mysqli_fetch_assoc($res);
+    if(!$row) {
+      echo "calculate_winner.php:mysqli_fetch_assoc failed\r\n";
+      //die();
+    } else {
+      //echo "calculate_winner.php:mysqli_fetch_assoc success\r\n";
+    }
+
+    echo $row['NAME'] . "\r\n";
+    echo $row['EMAIL'] . "\r\n";
+    echo $row['TOKEN'] . "\r\n";
 
     // TODO
     // Update META with winning token and email
