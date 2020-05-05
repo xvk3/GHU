5e0f9f2f62ae73803b0b0dffa3f80d1dfd4bcea0
W <w@xvk3.net>
2020-05-05 09:16:35 +0100

    fixed title animation bug (not linked to /js/index.js)

--- results.php
+++ results.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+    <script src="/js/index.js"></script>
     <script>
     function secToHMS(seconds) {
       var hours = Math.floor(seconds / 60 / 60);
